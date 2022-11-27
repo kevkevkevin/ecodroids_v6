2b8e3e4f146a5473f35563adf90b72787baf873e
kevin <kevinbulfanisay1@gmail.com>
2022-11-27 06:37:26 +0300

    Latest Revision

--- js/contact.js
+++ js/contact.js
@@ -249,9 +249,9 @@ Function Contact Map
 				content: contentString
 			});	
 			var companyImage = new google.maps.MarkerImage('images/marker.png',
-				new google.maps.Size(58,63),<!-- Width and height of the marker -->
+				new google.maps.Size(58,63),
 				new google.maps.Point(0,0),
-				new google.maps.Point(35,20)<!-- Position of the marker -->
+				new google.maps.Point(35,20)
 			);
 			var companyPos = new google.maps.LatLng(43.270441,6.640888);	
 			var companyMarker = new google.maps.Marker({
